db2fe6a41ccaec7e86f23cb8ae1471aabbae0fcd
Radi Atanassov <radi@sharepoint.bg>
2022-06-09 17:17:02 +0300

    simple changes

---
+++ tests/OneBitSoftware.Utilities.OperationResultTests/OperationResultConstructorTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace OneBitSoftware.Utilities.OperationResultTests
+{
+    public class OperationResultConstructorTests
+    {
+        [Fact]
+        public void NewOperationResult_ShouldNotBeNull()
+        {
+            var sut = new OperationResult();
+
+            Assert.NotNull(sut);
+        }
+
+        [Fact]
+        public void NewOperationResultWithNullLogger_ShouldSucceed()
+        {
+            var sut = new OperationResult(null);
+
+            Assert.NotNull(sut);
+        }
+    }
+}
