8f6051b1fa4ce86248e61c0f6339f817dece5bf2
Radi Atanassov <radi@sharepoint.bg>
2022-06-09 11:18:54 +0300

    Added the first test project

---
+++ tests/OneBitSoftware.Utilities.OperationResultTests/OperationResultTests.cs
@@ -0,0 +1,16 @@
+﻿using OneBitSoftware.Utilities;
+using Xunit;
+
+namespace OneBitSoftware.Utilities.OperationResultTests
+{
+    public class OperationResultTests
+    {
+        [Fact]
+        public void NewOperationResult_ShouldSucced()
+        {
+            var operationResult = new OperationResult();
+
+            Assert.NotNull(operationResult);
+        }
+    }
+}
