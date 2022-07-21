96edb0b5b16e18a77c33f141244268229510c2ac
Radi Atanassov <radi@sharepoint.bg>
2022-07-21 16:33:30 +0300

    Made AppendErrorMessages obsolete

--- tests/OneBitSoftware.Utilities.OperationResultTests/OperationResultAppendErrorsTests.cs
+++ tests/OneBitSoftware.Utilities.OperationResultTests/OperationResultAppendErrorsTests.cs
@@ -61,6 +61,40 @@ public class OperationResultAppendErrorsTests
         var operationResultBase = new OperationResult<object>();
         operationResultBase.AppendError(message2, errorCode2, LogLevel.Debug, detail2);
 
+        // Act - AppendErrorMessages is to be removed
+        operationResultBase.AppendErrorMessages(operationResultTarget);
+
+        // Assert
+        Assert.False(operationResultBase.Success);
+        Assert.False(operationResultTarget.Success);
+        Assert.True(operationResultBase.Fail);
+        Assert.True(operationResultTarget.Fail);
+
+        Assert.Equal(3, operationResultBase.Errors.Count);
+        Assert.Equal(2, operationResultTarget.Errors.Count);
+
+        Assert.NotNull(operationResultBase.Errors.Single(r => r.Code.Equals(errorCode2)));
+        Assert.NotNull(operationResultBase.Errors.Single(r => r.Message.Equals(message2)));
+        Assert.NotNull(operationResultBase.Errors.Single(r => r.Details is not null && r.Details.Equals(detail2)));
+    }
+
+    [Fact]
+    public void AppendErrorsStringInt_ShouldListAllErrors()
+    {
+        // Arrange
+        var errorCode1 = 666;
+        var message1 = "Error";
+        var detail1 = "Detail";
+        var operationResultTarget = new OperationResult<int>();
+        operationResultTarget.AppendError(message1, errorCode1, LogLevel.Debug, detail1);
+        operationResultTarget.AppendException(new Exception(message1));
+
+        var errorCode2 = 669;
+        var message2 = "Error2";
+        var detail2 = "Detail2";
+        var operationResultBase = new OperationResult<string>();
+        operationResultBase.AppendError(message2, errorCode2, LogLevel.Debug, detail2);
+
         // Act
         operationResultBase.AppendErrors(operationResultTarget);
 
